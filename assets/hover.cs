d94db0d221bc90400dffacd684768c5e74899f73
Thomas Graham <thomas.graham.97@gmail.com>
2017-09-28 06:06:57 -0400

    fix

---
+++ assets/hover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hover : MonoBehaviour {
+
+	Vector3 startpos; public float magnitude, rate;
+	void Start () {
+		startpos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 goalpos = startpos + (Random.insideUnitSphere * magnitude);
+		this.transform.position = Vector3.Lerp ( this.transform.position, goalpos, Time.deltaTime * rate );
+	}
+}
---
+++ assets/swell.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class swell : MonoBehaviour 
+{
+	public float sinMult, sinRate;
+	float value = 0f;
+	void Update () 
+	{
+		value += (Time.deltaTime * sinRate);
+		this.transform.localScale = Vector3.one + (Vector3.one * ( Mathf.Sin ( value ) * sinMult ) );
+
+		if ( value > 360f ) { value = 0; }
+	}
+}
